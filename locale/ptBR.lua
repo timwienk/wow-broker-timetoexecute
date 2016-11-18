8a6fc52005121f3bee8a0357bc345bda6f0fef88
Tim Wienk <tim@wienk.name>
2016-11-18 16:00:40 +0100

    Temporary fix for broken localisations in Curse packager.

--- locale/ptBR.lua
+++ locale/ptBR.lua
@@ -3,4 +3,15 @@ local L = LibStub('AceLocale-3.0'):NewLocale(name, 'ptBR')
 
 if not L then return end
 
---@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+L["Core options"] = "Core options" -- Requires localization
+L["Estimation options"] = "Estimation options" -- Requires localization
+L["Exec now"] = "Exec now" -- Requires localization
+L["Execute percentage"] = "Execute percentage" -- Requires localization
+L["History time (seconds)"] = "History time (seconds)" -- Requires localization
+L["Max history count"] = "Max history count" -- Requires localization
+L["Open configuration interface"] = "Open configuration interface" -- Requires localization
+L["Refresh time (seconds)"] = "Refresh time (seconds)" -- Requires localization
+L["to Exec"] = "to Exec" -- Requires localization
+L["to Kill"] = "to Kill" -- Requires localization
+L["Use focus target"] = "Use focus target" -- Requires localization
+
